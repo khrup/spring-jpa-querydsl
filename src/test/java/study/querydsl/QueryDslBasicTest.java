53f6bb320b3666f1d8d6f697f17ff073f7c4c7a0
공수한 <ksh92315@pionnet.co.kr>
2022-12-14 22:12:34 +0900

    조인 - on절

--- src/test/java/study/querydsl/QueryDslBasicTest.java
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -231,5 +231,54 @@ public class QueryDslBasicTest {
 
     }
 
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL : select m, t from Member m left join m.team t on t.name = 'teamA'
+     */
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .join(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
 
+        List<Tuple> result2 = queryFactory
+                .select(member, team)
+                .from(member, team)
+                .where(member.team.id.eq(team.id), team.name.eq("teamA")).fetch();
+
+        for (Tuple member :
+                result) {
+            System.out.println("member = " + member);
+        }
+
+        for (Tuple member :
+                result2) {
+            System.out.println("member2 = " + member);
+        }
+
+    }
+
+    /**
+     * 연관관계 없는 엔티티 외부조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.team.id.eq(team.id))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
