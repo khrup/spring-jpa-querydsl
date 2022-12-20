9f98575731659e63b9c1be4f31ba79c290e8ae97
공수한 <ksh92315@pionnet.co.kr>
2022-12-20 23:34:27 +0900

    스프링 데이터 페이징 활용 2 - CountQuery 최적화

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,11 +2,13 @@ package study.querydsl.repository;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -114,7 +116,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 .limit(pageable.getPageSize()) //몇개 가져올 것인지
                 .fetch();
 
-        long totalCount = queryFactory
+        JPAQuery<Long> countQuery = queryFactory
                 .select(member.count())
                 .from(member)
                 .leftJoin(member.team, team)
@@ -123,10 +125,9 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
-                )
-                .fetchOne();
+                );
 
-        return new PageImpl<>(result, pageable, totalCount);
+        return PageableExecutionUtils.getPage(result, pageable, () -> countQuery.fetchOne());
     }
 
 
